8bab688498c28443f8dee74d142d723e1314d4cc
Felipe Tassis <felipe.detassis@gmail.com>
2024-05-07 17:21:50 -0300

    main | Campos do Formulário

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,15 +9,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      colors: {
-        felipe: '#333333',
-      },
       maxWidth: {
         app: '700px',
       },
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'max-content 1fr min-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
     },
   },
