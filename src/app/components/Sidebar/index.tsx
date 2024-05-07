c01cf924a274dd7352bdf4dc627e481ec8d48010
Felipe Tassis <felipe.detassis@gmail.com>
2024-05-07 14:44:04 -0300

    main | Criando o perfil do usuário

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -11,6 +11,8 @@ import {
   Users,
 } from 'lucide-react'
 import { NavItem } from './NavItem'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
+import { Profile } from './Profile'
 
 export function Sidebar() {
   return (
@@ -42,35 +44,11 @@ export function Sidebar() {
           <NavItem title="Settings" icon={Cog} />
         </nav>
 
-        <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-5">
-          <div className="space-y-1">
-            <span className="text-sm/5 font-medium text-violet-700">
-              Used space
-            </span>
-            <p className="text-sm/5 text-violet-500">
-              Your team has used 80% of your available space. Need more?
-            </p>
-          </div>
+        <UsedSpaceWidget />
 
-          <div className="h-2 rounded-full bg-violet-100">
-            <div className="h-2 w-4/5 rounded-full bg-violet-600" />
-          </div>
+        <div className="h-px bg-zinc-200" />
 
-          <div className="space-x-3">
-            <button
-              type="button"
-              className="text-sm font-medium text-violet-500 hover:text-violet-600"
-            >
-              Dismiss
-            </button>
-            <button
-              type="button"
-              className="text-sm font-medium text-violet-700 hover:text-violet-900"
-            >
-              Upgrade plan
-            </button>
-          </div>
-        </div>
+        <Profile />
       </div>
     </aside>
   )
