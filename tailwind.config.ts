f1f8ee7dd9178ba0e758719061892b1767565292
Felipe Tassis <felipe.detassis@gmail.com>
2024-05-08 14:24:31 -0300

    main | Animando abertura dos Selects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,21 +9,42 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+
       maxWidth: {
         app: '700px',
       },
+
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'max-content 1fr min-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
+
       borderWidth: {
         6: '6px',
       },
+
       colors: {
         violet: {
           25: '#fcfaff',
         },
+      },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translateY(-2px)' },
+          to: { opacity: '1' },
+        },
+        // slideUpAndFade: {
+        //   from: { opacity: '1' },
+        //   to: { opacity: '0' },
+        // },
+      },
+
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 450ms cubic-bezier(0.16, 1, 0.3, 1)',
+        // slideUpAndFade: 'slideUpAndFade 1s linear',
       },
     },
   },
