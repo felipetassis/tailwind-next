e9e9e4fd816b06785a2c0d24141cc8ed1f5e7c7f
Felipe Tassis <felipe.detassis@gmail.com>
2024-05-09 08:27:39 -0300

    main | Tema dark da sidebar

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -11,7 +11,8 @@ const button = tv({
   variants: {
     variant: {
       primary: 'bg-violet-600 text-white hover:bg-violet-700',
-      ghost: 'rounded-mg px-2 hover:bg-zinc-50 text-zinc-500 shadow-none',
+      ghost:
+        'rounded-mg px-2 hover:bg-zinc-50 dark:hover:bg-zinc-800 text-zinc-500 shadow-none dark:text-zinc-400',
       outline: 'border border-zinc-300 text-zinc-700 hover:bg-zinc-50',
     },
   },
--- src/app/components/Sidebar/Profile.tsx
+++ src/app/components/Sidebar/Profile.tsx
@@ -4,22 +4,21 @@ import { Button } from '../Button'
 export function Profile() {
   return (
     <div className="grid grid-cols-profile items-center gap-3">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src="https://github.com/felipetassis.png"
         className="h-10 w-10 rounded-full"
         alt="Image Profile"
       />
       <div className="flex flex-col truncate">
-        <span className="truncate text-sm font-semibold text-zinc-700">
+        <span className="truncate text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Felipe Tassis
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           felipe.tassis@gmail.com
         </span>
       </div>
-      <Button variant="ghost" type="button">
-        <LogOut className="h-5 w-5 text-zinc-500" />
+      <Button type="button" variant="ghost">
+        <LogOut className="h-5 w-5" />
       </Button>
     </div>
   )
